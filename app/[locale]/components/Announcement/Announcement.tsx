ecfd401bfe9864fae1920fcd1f556b699c006f9e
josserden <josserden@gmail.com>
2024-06-14 18:08:50 +0300

    fix: added Announcement template

--- app/[locale]/components/Announcement/Announcement.tsx
+++ app/[locale]/components/Announcement/Announcement.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 
+import { AnnouncementCard } from '@/app/[locale]/components/Announcement/components/AnnouncementCard/AnnouncementCard';
 import { Calendar } from '@/app/[locale]/components/Announcement/components/Calendar';
 import { Container } from '@/app/(shared)/components/ui/Container/Container';
 import { Section } from '@/app/(shared)/components/ui/Section';
@@ -14,15 +15,20 @@ export const Announcement: FC<LocaleProps> = async ({ locale }) => {
   return (
     <Section>
       <Container className="decorBorderBottom relative">
-        <Typography as="h2" className="mb-10 md:mb-[60px]">
-          {t('announcementTitle')}
-        </Typography>
+        <div className="grid gap-10 md:grid-cols-2 md:gap-[60px]">
+          <div className="">
+            <Typography as="h2" className="mb-10 md:mb-[60px]">
+              {t('announcementTitle')}
+            </Typography>
+            <span className="block w-full border-b border-black/20" />
+            <Calendar locale={locale} />
+          </div>
 
-        <span className="block w-full border-b border-black/20" />
-
-        <Calendar locale={locale} />
-
-        <div className="grid gap-10 md:grid-cols-2 md:gap-[60px]"></div>
+          <ul className="grid gap-y-10">
+            <AnnouncementCard />
+            <AnnouncementCard />
+          </ul>
+        </div>
       </Container>
     </Section>
   );
---
+++ app/[locale]/components/Announcement/components/AnnouncementCard/AnnouncementCard.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react';
+import Image from 'next/image';
+
+import { Typography } from '@/app/(shared)/components/ui/Typography';
+
+import AnnouncementImage from '@/public/images/announcement.jpg';
+
+type Props = {
+  image?: string;
+  date?: string;
+  title?: string;
+  description?: string;
+};
+
+export const AnnouncementCard: FC<Props> = async (
+  {
+    image = AnnouncementImage,
+    date = '12.02.2024',
+    title = 'Золоті руки',
+    description = `Спільні творчі проекти, з метою подальшого розвитку дрібного бізнесу: навчання
+  бісероплетіння, виготовлення сувенірів, свічок, підготовки та участь у ярмарках народної
+  творчості. За відповідних умов можливість отримання грантів для розвитку свого бізнесу.`,
+  },
+) => {
+  return (
+    <li className="md:flex md:items-stretch md:gap-x-10">
+      <div className="grid gap-y-4 pb-5">
+        <Typography as="span" className="text-sm font-light text-zinc-500">
+          {date}
+        </Typography>
+
+        <Typography as="h3">{title}</Typography>
+
+        <Typography className="line-clamp-5">{description}</Typography>
+      </div>
+
+      <div className="shrink-0 rounded">
+        <Image
+          src={image}
+          alt="new image"
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center',
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </div>
+    </li>
+  );
+};
