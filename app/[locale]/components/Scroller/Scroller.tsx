33c78be0b45ba5a20907bdcafb887e6303c9204d
josserden <josserden@gmail.com>
2024-06-14 10:12:37 +0300

    fix: updated scroller settings

--- app/[locale]/components/Scroller/Scroller.tsx
+++ app/[locale]/components/Scroller/Scroller.tsx
@@ -12,18 +12,34 @@ export const Scroller: FC = () => {
   return (
     <Section>
       <Swiper
-        spaceBetween={20}
-        slidesPerView="auto"
+        spaceBetween={10}
+        slidesPerView={1}
+        centeredSlides={true}
         autoplay={{ delay: 2500, disableOnInteraction: false }}
         modules={[Autoplay]}
         className="mx-auto max-w-[1440px]"
+        loop={true}
+        breakpoints={{
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+          1280: {
+            slidesPerView: 6,
+            spaceBetween: 20,
+          },
+        }}
       >
         {Array.from({ length: 10 }).map((_, index) => (
           <SwiperSlide
             key={index}
-            className="inline-flex h-[60px] w-[220px] items-center justify-center"
+            className="flex !h-[60px] !w-[220px] shrink-0 items-center justify-center"
           >
-            <PartnerLogo className="h-[60px] w-[220px]" />
+            <PartnerLogo className="h-[60px] w-[210px]" />
           </SwiperSlide>
         ))}
       </Swiper>
