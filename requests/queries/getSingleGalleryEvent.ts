3216014999f871acca3a98922a5bc4e6bb36af19
Nadiia Khoptiuk <holineinadia@gmail.com>
2024-10-14 22:24:04 +0300

    change pagination at reguest, gallery - 2

--- requests/queries/getSingleGalleryEvent.ts
+++ requests/queries/getSingleGalleryEvent.ts
@@ -2,7 +2,7 @@ import { gql } from 'graphql-request';
 
 export const getSingleGalleryEvent = gql`
   query ($locale: I18NLocaleCode, $slug: String) {
-    galleryEvents(locale: $locale, filters: { slug: { eq: $slug } }, pagination: { limit: 100 }) {
+    galleryEvents(locale: $locale, filters: { slug: { eq: $slug } }) {
       data {
         attributes {
           title
